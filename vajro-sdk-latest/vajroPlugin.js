9354451efd4d1181e192f2f2a8773a6e0fd418e7
saravanamurugan <32792397+saravanamurugan@users.noreply.github.com>
2023-05-02 14:43:19 +0530

    Update vajroPlugin.js

--- vajro-sdk-latest/vajroPlugin.js
+++ vajro-sdk-latest/vajroPlugin.js
@@ -1,8 +1,66 @@
 var vajroPlugin;
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
-/******/ 	// The require scope
-/******/ 	var __webpack_require__ = {};
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/utils/common.ts":
+/*!*****************************!*\
+  !*** ./src/utils/common.ts ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "getCombinedOfferConfig": () => (/* binding */ getCombinedOfferConfig)
+/* harmony export */ });
+var __spreadArray = (undefined && undefined.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+var getCombinedOfferConfig = function (configDetails) {
+    return configDetails.reduce(function (combinedDetails, configDetail) {
+        var combinedOfferConfig = combinedDetails.combinedOfferConfig, nonCombinedOfferConfig = combinedDetails.nonCombinedOfferConfig;
+        var combinedOffer = configDetail.combinedOffer;
+        if (combinedOffer) {
+            return __spreadArray(__spreadArray([], combinedOfferConfig, true), [configDetail], false);
+        }
+        return __spreadArray(__spreadArray([], nonCombinedOfferConfig, true), [configDetail], false);
+    }, { combinedOfferConfig: [], nonCombinedOfferConfig: [] });
+};
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
 /******/ 	
 /************************************************************************/
 /******/ 	/* webpack/runtime/define property getters */
@@ -35,6 +93,8 @@ var vajroPlugin;
 /******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
 /*!**********************!*\
   !*** ./src/index.ts ***!
   \**********************/
@@ -42,12 +102,18 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "flow": () => (/* binding */ flow)
 /* harmony export */ });
+/* harmony import */ var _utils_common__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./utils/common */ "./src/utils/common.ts");
+
 var flow = function (appContext, configSchema) {
     var cartLineItems = appContext.cartLineItems;
     var lineItems = cartLineItems.lineItems;
     console.log({ lineItems: lineItems, configSchema: configSchema });
+    var _a = (0,_utils_common__WEBPACK_IMPORTED_MODULE_0__.getCombinedOfferConfig)(configSchema), combinedOfferConfig = _a.combinedOfferConfig, nonCombinedOfferConfig = _a.nonCombinedOfferConfig;
+    console.log({ combinedOfferConfig: combinedOfferConfig, nonCombinedOfferConfig: nonCombinedOfferConfig });
     return "test";
 };
+
+})();
 
 vajroPlugin = __webpack_exports__;
 /******/ })()
