c6d5f0706f1c59765bea74f558f4cfe04d0ab574
saravanamurugan <32792397+saravanamurugan@users.noreply.github.com>
2023-05-17 13:00:03 +0530

    Create configSchema.js

---
+++ vajro-sdk-latest/configSchema.js
@@ -0,0 +1,355 @@
+const configSchema = [
+    {
+        "buyOfferType": "overAll",
+        "cartType": "count",
+        "cartValue": "100",
+        "combinedOffer": true,
+        "discountType": "percentage",
+        "discountValue": "1",
+        "displayText": "Test",
+        "getOfferType": "buyOffer",
+        "offerCategory": "percentageDiscount"
+    },
+    {
+        "buyOfferType": "products",
+        "buyProducts": [
+            {
+                "productId": "8086426747177",
+                "productName": "Leather Cut-Off",
+                "productPrice": 628,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-02_Ashley_22_50944_23923_1000x1000.jpg?v=1674465384",
+                "variantId": "44308211237161"
+            },
+            {
+                "productId": "8086426747177",
+                "productName": "Leather Cut-Off",
+                "productPrice": 628,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-02_Ashley_22_50944_23923_1000x1000.jpg?v=1674465384",
+                "variantId": "44308211269929"
+            },
+            {
+                "productId": "8086426747177",
+                "productName": "Leather Cut-Off",
+                "productPrice": 628,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-02_Ashley_22_50944_23923_1000x1000.jpg?v=1674465384",
+                "variantId": "44308211302697"
+            },
+            {
+                "productId": "8086426747177",
+                "productName": "Leather Cut-Off",
+                "productPrice": 628,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-02_Ashley_22_50944_23923_1000x1000.jpg?v=1674465384",
+                "variantId": "44308211335465"
+            },
+            {
+                "productId": "8086426747177",
+                "productName": "Leather Cut-Off",
+                "productPrice": 628,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-02_Ashley_22_50944_23923_1000x1000.jpg?v=1674465384",
+                "variantId": "44308211368233"
+            }
+        ],
+        "cartType": "count",
+        "cartValue": 2,
+        "combinedOffer": true,
+        "discountType": "free",
+        "discountValue": 0,
+        "displayText": "",
+        "getOfferType": "products",
+        "getProductCount": 1,
+        "getProducts": [
+            {
+                "productId": "8086425534761",
+                "productName": "Auralias Leather Top",
+                "productPrice": 698,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-06-11-Ashley_Look5_50094_23460_copy_1000x1000.jpg?v=1674465315",
+                "variantId": "44308207042857"
+            }
+        ],
+        "offerCategory": "automaticOffers",
+        "reccuringFreeProduct": false
+    },
+    {
+        "buyOfferType": "products",
+        "buyProducts": [
+            {
+                "productId": "8086430253353",
+                "productName": "Classic Brogue",
+                "productPrice": 678,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-08_Laydown_51238_24354_1000x1000.jpg?v=1674465613",
+                "variantId": "44308222050601"
+            },
+            {
+                "productId": "8086430253353",
+                "productName": "Classic Brogue",
+                "productPrice": 678,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-08_Laydown_51238_24354_1000x1000.jpg?v=1674465613",
+                "variantId": "44308222083369"
+            },
+            {
+                "productId": "8086430253353",
+                "productName": "Classic Brogue",
+                "productPrice": 678,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-08_Laydown_51238_24354_1000x1000.jpg?v=1674465613",
+                "variantId": "44308222116137"
+            },
+            {
+                "productId": "8086430253353",
+                "productName": "Classic Brogue",
+                "productPrice": 678,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-08_Laydown_51238_24354_1000x1000.jpg?v=1674465613",
+                "variantId": "44308222148905"
+            },
+            {
+                "productId": "8086430253353",
+                "productName": "Classic Brogue",
+                "productPrice": 678,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-08_Laydown_51238_24354_1000x1000.jpg?v=1674465613",
+                "variantId": "44308222181673"
+            },
+            {
+                "productId": "8086430155049",
+                "productName": "Wrapped Golf Shoe",
+                "productPrice": 678,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-08_Laydown_51238_24379_1000x1000.jpg?v=1674465607",
+                "variantId": "44308221722921"
+            },
+            {
+                "productId": "8086430155049",
+                "productName": "Wrapped Golf Shoe",
+                "productPrice": 678,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-08_Laydown_51238_24379_1000x1000.jpg?v=1674465607",
+                "variantId": "44308221755689"
+            },
+            {
+                "productId": "8086430155049",
+                "productName": "Wrapped Golf Shoe",
+                "productPrice": 678,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-08_Laydown_51238_24379_1000x1000.jpg?v=1674465607",
+                "variantId": "44308221788457"
+            },
+            {
+                "productId": "8086430155049",
+                "productName": "Wrapped Golf Shoe",
+                "productPrice": 678,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-08_Laydown_51238_24379_1000x1000.jpg?v=1674465607",
+                "variantId": "44308221821225"
+            },
+            {
+                "productId": "8086430155049",
+                "productName": "Wrapped Golf Shoe",
+                "productPrice": 678,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-08_Laydown_51238_24379_1000x1000.jpg?v=1674465607",
+                "variantId": "44308221853993"
+            },
+            {
+                "productId": "8086430023977",
+                "productName": "Donna Low Top",
+                "productPrice": 328,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-08_Laydown_51242_24398_1000x1000.jpg?v=1674465601",
+                "variantId": "44308221493545"
+            },
+            {
+                "productId": "8086430023977",
+                "productName": "Donna Low Top",
+                "productPrice": 328,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-08_Laydown_51242_24398_1000x1000.jpg?v=1674465601",
+                "variantId": "44308221526313"
+            },
+            {
+                "productId": "8086430023977",
+                "productName": "Donna Low Top",
+                "productPrice": 328,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-08_Laydown_51242_24398_1000x1000.jpg?v=1674465601",
+                "variantId": "44308221559081"
+            },
+            {
+                "productId": "8086430023977",
+                "productName": "Donna Low Top",
+                "productPrice": 328,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-08_Laydown_51242_24398_1000x1000.jpg?v=1674465601",
+                "variantId": "44308221591849"
+            },
+            {
+                "productId": "8086430023977",
+                "productName": "Donna Low Top",
+                "productPrice": 328,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-08_Laydown_51242_24398_1000x1000.jpg?v=1674465601",
+                "variantId": "44308221624617"
+            }
+        ],
+        "cartType": "count",
+        "cartValue": "100",
+        "combinedOffer": true,
+        "discountType": "percentage",
+        "discountValue": "30",
+        "displayText": "30% off some products",
+        "getOfferType": "products",
+        "getProducts": [
+            {
+                "productId": "8086430253353",
+                "productName": "Classic Brogue",
+                "productPrice": 678,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-08_Laydown_51238_24354_1000x1000.jpg?v=1674465613",
+                "variantId": "44308222050601"
+            },
+            {
+                "productId": "8086430253353",
+                "productName": "Classic Brogue",
+                "productPrice": 678,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-08_Laydown_51238_24354_1000x1000.jpg?v=1674465613",
+                "variantId": "44308222083369"
+            },
+            {
+                "productId": "8086430253353",
+                "productName": "Classic Brogue",
+                "productPrice": 678,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-08_Laydown_51238_24354_1000x1000.jpg?v=1674465613",
+                "variantId": "44308222116137"
+            },
+            {
+                "productId": "8086430253353",
+                "productName": "Classic Brogue",
+                "productPrice": 678,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-08_Laydown_51238_24354_1000x1000.jpg?v=1674465613",
+                "variantId": "44308222148905"
+            },
+            {
+                "productId": "8086430253353",
+                "productName": "Classic Brogue",
+                "productPrice": 678,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-08_Laydown_51238_24354_1000x1000.jpg?v=1674465613",
+                "variantId": "44308222181673"
+            },
+            {
+                "productId": "8086430155049",
+                "productName": "Wrapped Golf Shoe",
+                "productPrice": 678,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-08_Laydown_51238_24379_1000x1000.jpg?v=1674465607",
+                "variantId": "44308221722921"
+            },
+            {
+                "productId": "8086430155049",
+                "productName": "Wrapped Golf Shoe",
+                "productPrice": 678,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-08_Laydown_51238_24379_1000x1000.jpg?v=1674465607",
+                "variantId": "44308221755689"
+            },
+            {
+                "productId": "8086430155049",
+                "productName": "Wrapped Golf Shoe",
+                "productPrice": 678,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-08_Laydown_51238_24379_1000x1000.jpg?v=1674465607",
+                "variantId": "44308221788457"
+            },
+            {
+                "productId": "8086430155049",
+                "productName": "Wrapped Golf Shoe",
+                "productPrice": 678,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-08_Laydown_51238_24379_1000x1000.jpg?v=1674465607",
+                "variantId": "44308221821225"
+            },
+            {
+                "productId": "8086430155049",
+                "productName": "Wrapped Golf Shoe",
+                "productPrice": 678,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-08_Laydown_51238_24379_1000x1000.jpg?v=1674465607",
+                "variantId": "44308221853993"
+            },
+            {
+                "productId": "8086430023977",
+                "productName": "Donna Low Top",
+                "productPrice": 328,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-08_Laydown_51242_24398_1000x1000.jpg?v=1674465601",
+                "variantId": "44308221493545"
+            },
+            {
+                "productId": "8086430023977",
+                "productName": "Donna Low Top",
+                "productPrice": 328,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-08_Laydown_51242_24398_1000x1000.jpg?v=1674465601",
+                "variantId": "44308221526313"
+            },
+            {
+                "productId": "8086430023977",
+                "productName": "Donna Low Top",
+                "productPrice": 328,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-08_Laydown_51242_24398_1000x1000.jpg?v=1674465601",
+                "variantId": "44308221559081"
+            },
+            {
+                "productId": "8086430023977",
+                "productName": "Donna Low Top",
+                "productPrice": 328,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-08_Laydown_51242_24398_1000x1000.jpg?v=1674465601",
+                "variantId": "44308221591849"
+            },
+            {
+                "productId": "8086430023977",
+                "productName": "Donna Low Top",
+                "productPrice": 328,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-08_Laydown_51242_24398_1000x1000.jpg?v=1674465601",
+                "variantId": "44308221624617"
+            }
+        ],
+        "offerCategory": "percentageDiscount"
+    },
+    {
+        "buyOfferType": "products",
+        "buyProducts": [
+            {
+                "productId": "8086428680489",
+                "productName": "Boy Tee",
+                "productPrice": 88,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-02_Ashley_14_50952_23837_1000x1000.jpg?v=1674465504",
+                "variantId": "44308217626921"
+            },
+            {
+                "productId": "8086428680489",
+                "productName": "Boy Tee",
+                "productPrice": 88,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-02_Ashley_14_50952_23837_1000x1000.jpg?v=1674465504",
+                "variantId": "44308217659689"
+            },
+            {
+                "productId": "8086428680489",
+                "productName": "Boy Tee",
+                "productPrice": 88,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-02_Ashley_14_50952_23837_1000x1000.jpg?v=1674465504",
+                "variantId": "44308217692457"
+            },
+            {
+                "productId": "8086428680489",
+                "productName": "Boy Tee",
+                "productPrice": 88,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-02_Ashley_14_50952_23837_1000x1000.jpg?v=1674465504",
+                "variantId": "44308217725225"
+            },
+            {
+                "productId": "8086428680489",
+                "productName": "Boy Tee",
+                "productPrice": 88,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/2015-07-02_Ashley_14_50952_23837_1000x1000.jpg?v=1674465504",
+                "variantId": "44308217757993"
+            }
+        ],
+        "cartType": "count",
+        "cartValue": 2,
+        "combinedOffer": true,
+        "discountType": "free",
+        "discountValue": 0,
+        "displayText": "",
+        "getOfferType": "products",
+        "getProductCount": 1,
+        "getProducts": [
+            {
+                "productId": "8086428418345",
+                "productName": "Ballet Top",
+                "productPrice": 78,
+                "url": "https://cdn.shopify.com/s/files/1/0709/5084/6761/products/babyandco3_1000x1000.jpg?v=1674465487",
+                "variantId": "44308216676649"
+            }
+        ],
+        "offerCategory": "buyXGetY"
+    }
+];
