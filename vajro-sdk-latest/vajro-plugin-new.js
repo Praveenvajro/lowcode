59a1062e6c75df453b57eec0e1875245213a8a54
saravanamurugan <32792397+saravanamurugan@users.noreply.github.com>
2023-05-23 18:00:01 +0530

    Update vajro-plugin-new.js

--- vajro-sdk-latest/vajro-plugin-new.js
+++ vajro-sdk-latest/vajro-plugin-new.js
@@ -683,7 +683,7 @@ var flow = function (appContext, configSchema) {
         discountPrice: discountPrice,
         offerAppliedDetails: offerAppliedDetails,
         offerRemovedDetails: offerRemovedDetails,
-        displayTextHtml: "test"
+        displayTextHtml: "<!DOCTYPE html>\n        <html lang=\"en\">\n            <head>\n                <meta charset=\"UTF-8\" />\n                <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" />\n                <meta name=\"viewport\" content=\"width=device-width,initial-scale=1.0\" />\n            </head>\n            <body style=\"min-width: auto; min-height: auto\">\n                <h2 style=\"text-align: center;\"><b>Test</b></h2>\n            </body>\n        </html>"
     };
     alert(JSON.stringify({ finalResult: finalResult }));
     return finalResult;
